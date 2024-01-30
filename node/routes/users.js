683d5b08733290b2dd165b6c868c6955d789afca
david319 <larapineda2003@gmail.com>
2024-01-29 18:33:27 -0600

    Weather Api

--- node/routes/users.js
+++ node/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
-var router = express.Router();
+const axios = require("axios");
+const router = express.Router();
+require('dotenv').config();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/weather', function (req, res, next) {
+    try {
+        axios.get("https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid="
+            + process.env.WHEATHER_API)
+            .then(response => {
+                res.send(response.data);
+            })
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 module.exports = router;
